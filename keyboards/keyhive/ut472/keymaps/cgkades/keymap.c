0c5a313bb3d9846a7f531e886724d07a2bc4ea9b
Brett Yoakum <yoakum@adobe.com>
2023-01-31 17:42:40 -0700

    Updated the ut47.2

--- keyboards/keyhive/ut472/keymaps/cgkades/keymap.c
+++ keyboards/keyhive/ut472/keymaps/cgkades/keymap.c
@@ -41,9 +41,9 @@ LAYOUT(
   KC_LCTL, KC_LALT, KC_LGUI, KC_APP,  MO(2),      KC_SPC,        MO(1),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
-  /* FN Layer 1
+  /* FN Layer 1 Raise
    * ,-------------------------------------------------------------------------.
-   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Delete|
+   * |   ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |Delete|
    * |-------------------------------------------------------------------------+
    * |      | RGB |RGBMO|BRT+ |BRT- |     |     |  -  |  =  |  [  |  ]  |  \   |
    * |-------------------------------------------------------------------------+
@@ -54,15 +54,15 @@ LAYOUT(
    */
 
 LAYOUT( /* Right */
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
+  KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
   _______, RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______,
   _______, _______, _______, KC_CAPS, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
-  /* FN Layer 2
+  /* FN Layer 2 Lower
    * ,-------------------------------------------------------------------------.
-   * |   ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |Delete|
+   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Delete|   
    * |-------------------------------------------------------------------------+
    * |      |     |     |     |     |     |     |  _  |  +  |  { |  }  |   |   |
    * |-------------------------------------------------------------------------+
@@ -73,13 +73,13 @@ LAYOUT( /* Right */
    */
 
 LAYOUT( /* Left */
-  KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
   _______, _______, _______, _______, _______, _______, _______, KC_UNDERSCORE, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
   _______, _______, _______, KC_CAPS, _______,     _______,      _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
-  /* FN Layer 3
+  /* FN Layer 3 Adjust
    * ,-------------------------------------------------------------------------.
    * | Esc | Calc|Webhm| Mail| Comp|     |     |     |     |     |PrtSc|       |
    * |-------------------------------------------------------------------------+
