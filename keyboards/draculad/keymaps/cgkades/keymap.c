f491ae4d0166c60431e2366d84df67a0f589c50f
cgkades <cgkades@gmail.com>
2023-02-27 17:39:27 -0700

    matched draculad to crowboard

--- keyboards/draculad/keymaps/cgkades/keymap.c
+++ keyboards/draculad/keymaps/cgkades/keymap.c
@@ -28,35 +28,35 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] =  LAYOUT(
         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                                             KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_ENT),
+        LCTL_T(KC_A),         KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOTE,
+        LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                                             KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
                                                  KC_MUTE,                                          TG(_ADJ),
-                                        KC_LCTL, KC_LGUI, LT(_NUM,KC_BSPC),    LT(_SYMB,KC_SPACE), LT(_MUS,KC_ENT), KC_LALT
+                                        KC_LGUI, KC_LALT, LT(_NUM,KC_BSPC),    LT(_SYMB,KC_SPACE), LT(_MUS,KC_ENT), KC_RCTL
     ),
     [_NUM] = LAYOUT(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_TAB,  XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-        KC_LSFT, XXXXXXX, XXXXXXX, KC_MUTE, QK_BOOT,                      KC_HOME, KC_END,  KC_PGUP, KC_PGDN, KC_RSFT,
+        KC_TAB,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,                      XXXXXXX, KC_MINUS, KC_EQUAL,   KC_LBRC, KC_RBRC,
+        KC_LCTL, KC_GRV, XXXXXXX, KC_MUTE, XXXXXXX,                      KC_HOME, KC_END,  XXXXXXX, KC_BSLS, KC_SCLN,
                                             XXXXXXX,                      KC_NO,
-                                   XXXXXXX, KC_LALT, _______,    _______, KC_ENT,  KC_NO
+                                   XXXXXXX, XXXXXXX, _______,    KC_ENT, _______,  KC_NO
     ),
     [_SYMB] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,                        XXXXXXX, XXXXXXX, XXXXXXX, KC_EQL,  KC_MINS,
-        KC_ESC, KC_F5,   KC_F6,   KC_F7,   KC_F8,                        KC_LBRC, KC_RBRC, XXXXXXX, KC_GRV,  KC_BSLS,
-        KC_LSFT, KC_F9,   KC_F10,  KC_F11,  KC_F12,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
+        LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5),       LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0),
+        KC_ESC,  KC_DEL,  LCTL(KC_A),   XXXXXXX,   XXXXXXX,                   XXXXXXX, LSFT(KC_MINUS), LSFT(KC_EQUAL),   LSFT(KC_LBRC), LSFT(KC_RBRC),
+        KC_LSFT, LSFT(KC_GRV),   XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, LSFT(KC_BSLS), LSFT(KC_SCLN),
                                             XXXXXXX,                      KC_NO,
-                                   KC_LALT, XXXXXXX, _______,    _______, XXXXXXX, KC_NO
+                                   XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX, KC_NO
     ),
     [_MUS] = LAYOUT(
-        KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_LALT, KC_BTN3, KC_BTN2, KC_BTN1, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+        KC_F11,  KC_F12,  KC_BTN2, KC_BTN1, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
         KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                             XXXXXXX,                      XXXXXXX,
                                    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, _______, XXXXXXX
     ),
     [_ADJ] = LAYOUT(
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        EE_CLR,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     RGB_MOD,  RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG,
+        EE_CLR,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      RGB_MOD,  RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______,
                                             XXXXXXX,                      _______,
                                    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
--- keyboards/keyhive/ut472/keymaps/cgkades/keymap.c
+++ keyboards/keyhive/ut472/keymaps/cgkades/keymap.c
@@ -38,7 +38,7 @@ LAYOUT(
   LT3_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
-  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(2),      KC_SPC,        MO(1),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LT(2, KC_BSPC),   KC_SPC, LT(1, KC_SPC),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
   /* FN Layer 1 Raise
