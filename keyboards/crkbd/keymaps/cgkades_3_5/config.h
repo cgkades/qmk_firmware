6c513a27c57d6cd857ed2d3a0c4c2e5ea6c1ff4b
Brett Yoakum <yoakum@adobe.com>
2023-08-24 19:19:37 -0600

    updating changes

--- keyboards/crkbd/keymaps/cgkades_3_5/config.h
+++ keyboards/crkbd/keymaps/cgkades_3_5/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 //#define TAPPING_FORCE_HOLD
 //#define TAPPING_TERM 100
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
--- keyboards/eyeohdesigns/babyv/keymaps/linuxMain/keymap.c
+++ keyboards/eyeohdesigns/babyv/keymaps/linuxMain/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, KC_QWERTY, KC_GAMES, XXXXXXX,  XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD,  KC_PGDN,
         CG_TOGG,                      KC_TRNS,        XXXXXXX,              XXXXXXX,      KC_TRNS,                    QK_BOOT
     ),
-    [_NUMBRS] = LAYOUT_2u(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,      KC_F6,         KC_F7,   KC_F8,   KC_F9,     KC_F10,   KC_F11,  KC_F12,
+    [_NUMBRS] = LAYOUT_2u(  
         XXXXXXX, KC_1,    KC_2,    KC_3,      KC_4,       KC_5,          KC_6,    KC_7,    KC_8,      KC_9,     KC_0,    KC_PIPE,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,      KC_F6,         KC_F7,   KC_F8,   KC_F9,     KC_F10,   KC_F11,  KC_F12,
         XXXXXXX, KC_MINS, KC_EQL,   KC_HASH,   KC_LBRC,    KC_RBRC,       KC_LCBR, KC_RCBR, KC_ASTR,   KC_LPRN,  KC_RPRN, KC_RSFT,
         KC_LCTL,                   KC_TRNS,         XXXXXXX,                 XXXXXXX,       KC_TRNS,                     QK_BOOT
     ),
